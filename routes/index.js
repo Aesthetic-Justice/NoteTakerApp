cb1e8f1fcd3591d2a2c92af0ebbfc6aca4b2904d
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2022-12-17 11:38:08 -0500

    Cleaned up project

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require(`express`);
 
 const apiRoutes = require(`./notes`);
-const pageRoutes = require(`./pages`);
 
 const app = express();
 
 app.use(`/notes`,apiRoutes);
-app.use(`/`,pageRoutes);
 
 module.exports = app;
