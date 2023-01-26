c31fe5395aa64d672911a110adf14098ce5a2c39
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-01-26 14:37:14 -0500

    redirected to correct port for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require(`express`);
 const path = require('path');
 const api = require(`./routes/index`)
 
-const PORT = 3001;
+const PORT = process.env.PORT;
+if (PORT == null || PORT == "") {
+  PORT = 8000;
+}
 
 const app = express();
 
